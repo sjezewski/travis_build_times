b956e89322137b9b136e907599c516235325be54
Sean Jezewski <seanwjezewski@gmail.com>
2016-12-04 13:48:16 -0800

    Update graph to multiplot

--- graph.rb
+++ graph.rb
@@ -2,39 +2,47 @@
 
 require 'gnuplot'
 
-if ARGV.size != 2
-    puts "Usage: ./graph.rb probablities.txt outputDir"
+if ARGV.size < 2
+    puts "Usage: ./graph.rb outputFilepath prob1.txt (prob2.txt) ..."
     exit 1
 end
 
-def plot(outputDir, filename)
-
+def valid_data(filename)
     data = File.read(filename).split("\n")
-    newFilename = "#{File.split(filename).last.split(".").first}-plot"
-    outputFile = File.join(outputDir, newFilename)
-File.open("#{outputFile}.dat", "w") do |gp|
-  Gnuplot::Plot.new( gp ) do |plot|
-    plot.terminal "gif"
-    plot.output "#{outputFile}.gif" 
+    data = data.collect {|d| d.to_f }
+    data.pop while data.last == 0
+    data
+end 
 
-    plot.xrange "[0:#{data.size}]"
+def plot(outputFilename, filenames)
+    dataSets = filenames.collect {|fn| valid_data(fn)}
+    maxX = 0
+    dataSets.each {|data| maxX = data.size if data.size > maxX}
+    outputFilename = outputFilename.split(".").first
+File.open("#{outputFilename}.dat", "w") do |gp|
+  Gnuplot::Plot.new( gp ) do |plot|
+    plot.terminal "png"
+    plot.output "#{outputFilename}.png" 
+
+    plot.xrange "[0:#{maxX}]"
     plot.title  "Prob of timeout given current build time"
     plot.ylabel "Probability"
     plot.xlabel "Minute"
     
-    x = (1..data.size).collect { |v| v.to_f }
-    y = data.collect {|z| z.to_f}
-
-    plot.data = [
-      Gnuplot::DataSet.new( [x, y] ) { |ds|
-        ds.with = "linespoints"
-        ds.title = "Array data"
-      }
-    ]
+    plots = dataSets.collect do |data|
+        x = (1..data.size).collect { |v| v.to_f }
+        y = data
+    
+        Gnuplot::DataSet.new( [x, y] ) { |ds|
+          ds.with = "linespoints"
+          ds.title = "Array data"
+        }
+    end
+    plot.data = plots
 
   end
 end
 
 end
 
-plot(ARGV[1], ARGV[0])
+plot(ARGV[0], ARGV[1..-1])
