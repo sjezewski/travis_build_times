ebcf5115ddce58a96783d942d474d5746a2ee867
Sean Jezewski <seanwjezewski@gmail.com>
2016-12-04 12:06:00 -0800

    Outline analysis approach

--- analyze.rb
+++ analyze.rb
@@ -1,7 +1,20 @@
 #!/usr/bin/env ruby
 
 # Given a set of data (via a single input file), computes the likelihood
-# that a build will timeout given its age at each minute
+# that a build will timeout given its age. We'll do this for each minute
+# so that we can see how this likelihood changes. Ideally, we set our
+# timeouts to match the 50% mark
+
+
+# A : timeout
+# B_x : build takes _at_least_ x minutes
+#
+# P(A|B_x) = P(B_x|A)P(A)/P(B_x)
+#          = likelihood a build will timeout given its made it to at least X minutes
+#
+# P(B_x|A) = of the builds that timeout, how many made it to at least x minutes?
+# P(A)     = global likelihood of timeout
+# P(B_x)   = global likelihood of a build making it to x minutes
 
 def classify(build)
     if build['duration'] == 0
@@ -17,14 +30,17 @@ def classify(build)
     return :finished
 end
 
-
-def counts(builds)
+# Its important we traverse in order, so that we can calculate P(B_x|A)
+def frequencies(builds)
     frequencies = {}
     builds.each do |build|
         klass = classify(build)
-        frequencies[klass] = 0 if frequencies[klass].nil?
-        frequencies[klass] += 1
+        frequencies[klass] = [] if frequencies[klass].nil?
+        frequencies[klass] << build
     end
 
     frequencies
 end
+
+
+
